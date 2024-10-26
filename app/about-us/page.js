1eab3b02bfbd6b546b14dd98b3b6e64bd83e323d
Ogaga Wisdom <wizdomogaga@gmail.com>
2024-10-26 15:48:09 +0100

    install part of homepage

---
+++ app/about-us/page.js
@@ -0,0 +1,9 @@
+
+export default function AboutUs () {
+    return(
+        <div>
+            <h1>About Wholesalers NG</h1>
+            <p>lorem sjdkjs jsh yurur rt jsj did si gfa ur uiehe </p>
+        </div>
+    )
+}
